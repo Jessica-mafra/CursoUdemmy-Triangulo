f94f328e4c83b648710ba37ae56424a3b23bbfae
Jessica-mafra <119601801+Jessica-mafra@users.noreply.github.com>
2023-02-03 13:16:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using CursoUdemmy2;
+{
+    Triangulo x, y;
+    x= new Triangulo();
+    y= new Triangulo(); 
+
+    Console.Write("Entre com as medidas do triângulo X:");
+    x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Console.Write("Eentre com as medidas do triângulo Y:");
+    y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+     y.B= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+    double areaX = x.Area();// classe atribuida triangulo
+
+    double areaY = y.Area();//classe atribuida triangulo
+
+    Console.WriteLine("Àrea de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+    Console.WriteLine("Àrea de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+    if (areaX > areaY)
+    {
+        Console.WriteLine("Maior àrea: X");
+    }
+    else
+    {
+        Console.WriteLine("Maior àrea : Y");
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CursoUdemmy2// name space da classe
+{
+  class Triangulo// nome da classe
+    {
+        //atributos da classe
+        public double A;
+        public double B;
+        public double C;
+
+
+        //public indica que atributo ou metodo pode ser usado em outros arquivos
+        public double Area() { //nome do metodo,lista de parametros do metodo, tipo de daodo que o metodo retorna
+            double p = (A + B + C) / 2.0;
+            return  Math.Sqrt(p * (p-A) * (p-B) * (p-C));//formula de heron, corpo do metodo
+            
+        }
+    }
+}
